6a4f60a5a5e29ebc8bd275ee5186a47235de5754
sharmiladommaraju <77896994+sharmiladommaraju@users.noreply.github.com>
2021-02-05 15:25:16 -0600

    javaprogram

---
+++ myprgm.java
@@ -0,0 +1,18 @@
+class  MyPrg1{
+ public static void main(String args[]){
+     int a = 100;
+     System.out.println("Bulid triiger on 02/04 Second attempt !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Hello World from a git Hub 0........ 1201 Jenkins Trigger");
+     System.out.println("Hello World from a git Hub 1........ 0322 Jenkins Trigger");
+     System.out.println("Hello World from a git Hub 1........"+a);
+     System.out.println("Hello World from a git Hub 2");
+     System.out.println("12 New change added to the Java program");
+     System.out.println("Change added to trigger a SCM polled job");
+     System.out.println("Change added to trigger jenkins job 03224");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! New Line 0520  !!!!!!!!!!!!!!!!!!!!!!!!1");
+     System.out.println("##################################### New Line 0520  ########################1");
+     System.out.println("##################################### New Line 1201  ########################1");
+ }
+}
+// Comment added to the program to trigger Jenkins job
+// Comment added to the program to trigger Jenkins qw
